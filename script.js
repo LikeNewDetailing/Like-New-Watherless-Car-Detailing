b03caa44f9321ad902a9a26c23f25be78269c6a9
Like New <likenewca@gmail.com>
2025-07-07 12:05:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -71,5 +71,12 @@ function toggleMenu() {
     });
   });
 </script>
+const menuBtn = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
 
 
